444f754abe79f31c90a5cbcf19189563ed671ee8
notchwis <kodenothing@gmail.com>
2026-05-27 21:33:15 -0500

    Create camera.h

---
+++ src/camera.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "raylib.h"
+
+typedef enum {
+    CAM_FIRST_PERSON = 1,
+    CAM_THIRD_CLOSE  = 2,
+    CAM_THIRD_FAR    = 3
+} CamMode;
+
+extern Camera3D cam;
+extern CamMode camMode;
+
+void InitGameCamera();
+void UpdateCamera();
